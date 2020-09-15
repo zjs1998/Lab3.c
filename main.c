c35b5476dbd752dc60c3dd2a93babca9106f204b
Jingsheng Zhang <zjs899171@gmail.com>
2020-09-15 20:12:50 +0000

    1

--- main.c
+++ main.c
@@ -7,17 +7,17 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
-
 int sum_n(int n){
-  if (n>=1){
-    n=n+sum_n(n-1);
-    }
-  else 
-    printf("sum is %c.\n",n);
+  int sum;
+    if (n==0)
+      sum=0;
+    else
+      sum=sum_n(n-1)+n;
+      return sum;
  }
 
 void print_n(const char *s, int n){
-   if (n>=0){
+   if (n>=1){
      printf("%s.\n",s);
      print_n(s,n-1);
    }
@@ -28,11 +28,10 @@ void print_n(const char *s, int n){
 int main() {
   char *name = readline("Enter an int:");
   int n = atof(name);
-  int sum_n(int n);
+  printf("sum is %d.\n",sum_n(n));
   char *s = readline("Enter a string: ");
   print_n(s,n);
-  printf("Hello World\n");
-  return 1;
+  return 0;
 }
 
 
